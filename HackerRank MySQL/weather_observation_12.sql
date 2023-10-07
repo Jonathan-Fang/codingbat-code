b3eaaa493571e3b864483c93655a6237fc4ced9d
Jonathan Fang <im.jonathan.fang@gmail.com>
2023-10-06 20:41:01 -0700

    solved weather observation station 12

--- HackerRank MySQL/weather_observation_12.sql
+++ HackerRank MySQL/weather_observation_12.sql
@@ -1,4 +1,22 @@
 -- Prompt: Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
 -- Link: https://www.hackerrank.com/challenges/weather-observation-station-12/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
--- Status: 
+-- Status: Solved October 6, 2023
 
+-- Use-Cases
+-- X aa&bb
+-- X aa&aa
+-- X bb&aa
+-- X bb&bb
+
+select DISTINCT CITY
+from STATION
+where (CITY not like 'a%'
+and CITY not like 'e%' 
+and CITY not like 'i%' 
+and CITY not like 'o%' 
+and CITY not like 'u%')
+and (CITY not like '%a'
+and CITY not like '%e'
+and CITY not like '%i'
+and CITY not like '%o'
+and CITY not like '%u');
