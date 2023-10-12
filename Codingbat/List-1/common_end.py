d7b105431db5f96bae620ee0264b799da8309a9e
Jonathan Fang <im.jonathan.fang@gmail.com>
2023-10-11 23:18:29 -0700

    solved one more exercise

---
+++ Codingbat/List-1/common_end.py
@@ -0,0 +1,4 @@
+# Prompt: Given 2 arrays of ints, a and b, return True if they have the same first element or they have the same last element. Both arrays will be length 1 or more.
+# Link: 
+# Status: 
+
---
+++ Codingbat/List-1/make_pi.py
@@ -0,0 +1,6 @@
+# Prompt: Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+# Link: https://codingbat.com/prob/p113659
+# Status: Solved October 11, 2023
+
+def make_pi():
+  return [3, 1, 4]
