3aa504a54ad2b0394ad8d9348814ebb5a4edaae1
Jonathan Fang <im.jonathan.fang@gmail.com>
2023-10-12 15:16:55 -0700

    solved list-1 module

--- Codingbat/List-1/common_end.py
+++ Codingbat/List-1/common_end.py
@@ -1,4 +1,6 @@
 # Prompt: Given 2 arrays of ints, a and b, return True if they have the same first element or they have the same last element. Both arrays will be length 1 or more.
-# Link: 
-# Status: 
+# Link: https://codingbat.com/prob/p147755
+# Status: Solved October 12, 2023
 
+def common_end(a, b):
+  return (a[0] == b[0] or a[len(a) - 1] == b[len(b) - 1])
---
+++ Codingbat/List-1/sum3.py
@@ -0,0 +1,6 @@
+# Prompt: Given an array of ints length 3, return the sum of all the elements.
+# Link: https://codingbat.com/prob/p191645
+# Status: Solved October 12, 2023
+
+def sum3(nums):
+  return nums[0] + nums[1] + nums[2]
