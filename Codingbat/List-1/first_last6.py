17f7061bbecaaa1cd7c4d2ce1950f94fdf2a027e
Jonathan Fang <im.jonathan.fang@gmail.com>
2023-10-11 19:32:54 -0700

    reorganized into codingbat folder

---
+++ Codingbat/List-1/first_last6.py
@@ -0,0 +1,5 @@
+# Prompt: Given an array of ints, return True if 6 appears as either the first or last element in the array. The array will be length 1 or more.
+# Link: https://codingbat.com/prob/p181624
+# Status: 
+
+ 
