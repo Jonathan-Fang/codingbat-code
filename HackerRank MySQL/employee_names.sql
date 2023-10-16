41866016930d96bd6646a99752efdec85f2a9e56
Jonathan Fang <im.jonathan.fang@gmail.com>
2023-10-16 00:21:37 -0700

    finished another hackerrank problem for today

--- HackerRank MySQL/employee_names.sql
+++ HackerRank MySQL/employee_names.sql
@@ -1,3 +1,5 @@
+-- Prompt: Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
 -- Link: https://www.hackerrank.com/challenges/name-of-employees/problem?isFullScreen=true
+-- Status: Solved before October 16, 2023
 
 select name from Employee order by name ASC;
